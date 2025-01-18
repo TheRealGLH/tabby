5bf1453f9c2e0c25ce1d0411ed0d7e102332167d
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2025-01-18 10:45:16 +0100

    basic settings and selecting folders

--- src/script.js
+++ src/script.js
@@ -5,13 +5,34 @@ init();
 function init() {
     let bookmarks = browser.bookmarks.getTree();
     bookmarks.then(onBookmarks, onRejected);
+    document.getElementById("settings-button").onclick = onSettingsToggle;
+    let select = document.getElementById("settings-favourites");
+    select.onchange = function() {
+        onSettingsFaveSelect(select);
+    };
 }
 
 
 function onBookmarks(bookmarks) {
+    let settingsSelectElement = document.getElementById("settings-favourites");
     bookmarks.forEach(bookmarkTree => {
         renderBookmarkTreeItem(bookmarkTree, container);
+        populateFolderFaves(bookmarkTree, settingsSelectElement);
     });
+}
+
+
+function populateFolderFaves(bookmarkTree, selectElement) {
+    if (bookmarkTree.type == "folder") {
+        bookmarkTree.children.forEach(treeChild => {
+            populateFolderFaves(treeChild, selectElement);
+        });
+        let option = document.createElement("option");
+        option.className = "settings-favourites-option";
+        option.value = bookmarkTree.id;
+        option.innerText = bookmarkTree.title;
+        selectElement.appendChild(option);
+    }
 }
 
 function renderBookmarkTreeItem(bookmarkTreeItem, parentElement) {
@@ -38,5 +59,27 @@ function renderBookmarkTreeItem(bookmarkTreeItem, parentElement) {
 
 
 function onRejected(error) {
-    console.log(error);
+    console.error(error);
+}
+
+function onSettingsToggle() {
+    let menu = document.getElementById("settings-menu");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
+
+function onSettingsFaveSelect(selectElement) {
+    let selectedBookmarks = browser.bookmarks.getSubTree(selectElement.value);
+    selectedBookmarks.then(onBookmarkFaveSettingChange, onRejected);
+}
+
+function onBookmarkFaveSettingChange(bookmarks) {
+    container.querySelector(".bookmark-item-container").innerHTML = "";
+    bookmarks.forEach(bookmarkTree => {
+        renderBookmarkTreeItem(bookmarkTree, container);
+    });
 }
